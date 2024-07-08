80d93e9c6bb7cc63b8033623f0aa6ba12e3b7a66
toolxu1 <chojlaj3@gmail.com>
2024-07-08 22:13:04 +0700

    code

--- videocall/settings.py
+++ videocall/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9ndce&l@l+tiq0&hpd^9p!xki()!dka-l+ad+$cn)48v-%(ybr'
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', '*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
